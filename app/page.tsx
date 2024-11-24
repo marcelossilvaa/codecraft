784fa3c9176c4d921d993f62cc8d46f2526c20b3
Marcelo Silva <marcelo_810@hotmail.com>
2024-11-24 20:28:25 -0300

    feat/ trilha page

--- app/page.tsx
+++ app/page.tsx
@@ -1,160 +1,219 @@
-"use client";
-import Image, { StaticImageData } from "next/image";
-import { useState } from "react";
-import Code from "@/public/code.png";
-import Book from "@/public/book.png";
-import Bau from "@/public/bau.png";
-import Start from "@/public/start.png";
 
-interface Lesson {
-  level: number;
-  image: StaticImageData;
-}
 
-interface Unit {
-  unidade: number;
-  licoes: Lesson[];
-}
+import Image from 'next/image';
+import Filter from '@/public/filter.png';
+import Notificacoes from '@/public/notificacoes.png';
+import HomeIcon from '@/public/Home.png';
+import Chart from '@/public/Chart.png';
+import Chat from '@/public/chat.png';
+import Profile from '@/public/profile.png';
+import Link from 'next/link';
 
-const dataUnits: Unit[] = [
+const dataTrilhas = [
   {
-    unidade: 1,
-    licoes: [
-      { level: 1, image: Start },
-      { level: 2, image: Book },
-      { level: 3, image: Bau },
-      { level: 4, image: Code },
-      { level: 5, image: Code },
-    ],
+    id: 1,
+    nome: 'Javascript',
   },
   {
-    unidade: 2,
-    licoes: [
-      { level: 6, image: Code },
-      { level: 7, image: Code },
-      { level: 8, image: Code },
-      { level: 9, image: Code },
-      { level: 10, image: Code },
-    ],
+    id: 2,
+    nome: 'Python',
   },
   {
-    unidade: 3,
-    licoes: [
-      { level: 11, image: Code },
-      { level: 12, image: Code },
-      { level: 13, image: Code },
-      { level: 14, image: Code },
-      { level: 15, image: Code },
-    ],
+    id: 3,
+    nome: 'Java',
   },
-];
-
-interface LessonNodeProps {
-  lesson: Lesson;
-  onClick: (lesson: Lesson) => void;
-}
-
-const LessonNode = ({ lesson, onClick }: LessonNodeProps) => (
-  <div
-    onClick={() => onClick(lesson)}
-    className="lesson-node flex flex-col items-center mx-2 cursor-pointer"
-  >
-    <Image
-      src={lesson.image}
-      alt={`Lição ${lesson.level}`}
-      width={80}
-      height={80}
-    />
-  </div>
-);
+  {
+    id: 4,
+    nome: 'C#',
+  },
+  {
+    id: 5,
+    nome: 'Ruby',
+  },
+  {
+    id: 6,
+    nome: 'C++',
+  },
+  {
+    id: 7,
+    nome: 'PHP',
+  },
+  {
+    id: 8,
+    nome: 'Go',
+  },
+  {
+    id: 9,
+    nome: 'Swift',
+  },
+  {
+    id: 10,
+    nome: 'Kotlin',
+  },
+  {
+    id: 11,
+    nome: 'R',
+  },
+  {
+    id: 12,
+    nome: 'Typescript',
+  },
+  {
+    id: 13,
+    nome: 'Dart',
+  },
+  {
+    id: 14,
+    nome: 'Scala',
+  },
+  {
+    id: 15,
+    nome: 'Perl',
+  },
+  {
+    id: 16,
+    nome: 'Lua',
+  },
+  {
+    id: 17,
+    nome: 'Rust',
+  },
+  {
+    id: 18,
+    nome: 'Objective-C',
+  },
+  {
+    id: 19,
+    nome: 'Haskell',
+  },
+  {
+    id: 20,
+    nome: 'Shell',
+  },
+  {
+    id: 21,
+    nome: 'Pascal',
+  },
+  {
+    id: 22,
+    nome: 'Elixir',
+  },
+  {
+    id: 23,
+    nome: 'Clojure',
+  },
+  {
+    id: 24,
+    nome: 'Groovy',
+  },
+  {
+    id: 25,
+    nome: 'Perl',
+  },
+  {
+    id: 26,
+    nome: 'Lua',
+  },
+  {
+    id: 27,
+    nome: 'Rust',
+  },
+  {
+    id: 28,
+    nome: 'Objective-C',
+  },
+  {
+    id: 29,
+    nome: 'Haskell',
+  },
+  {
+    id: 30,
+    nome: 'Shell',
+  },
+  {
+    id: 31,
+    nome: 'Pascal',
+  },
+  {
+    id: 32,
+    nome: 'Elixir',
+  },
+  {
+    id: 33,
+    nome: 'Clojure',
+  },
+  {
+    id: 34,
+    nome: 'Groovy',
+  },
+  {
+    id: 35,
+    nome: 'Perl',
+  },
+  {
+    id: 36,
+    nome: 'Lua',
+  },
+  {
+    id: 37,
+    nome: 'Rust',
+  },
+  {
+    id: 38,
+    nome: 'Objective-C',
+  },
+  {
+    id: 39,
+    nome: 'Haskell',
+  },
+  {
+    id: 40,
+    nome: 'Shell',
+  },
+]
 
 export default function Home() {
-  const [selectedLesson, setSelectedLesson] = useState<Lesson | null>(null);
-
-  const handleLessonClick = (lesson: Lesson) => {
-    setSelectedLesson(lesson);
-  };
-
-  const handleBackToLessons = () => {
-    setSelectedLesson(null);
-  };
-
-  return (
-    <div className="mt-10 w-full flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4">CodeCraft</h1>
-
-      {selectedLesson ? (
-        <div className="lesson-content w-full max-w-md p-4">
-          <h2 className="text-xl font-semibold mb-4">
-            Conteúdo da Lição {selectedLesson.level}
-          </h2>
-          <p>
-            Este é o conteúdo da Lição {selectedLesson.level}. Aqui você pode
-            adicionar exercícios, explicações, exemplos de código ou qualquer
-            outro conteúdo relevante para esta lição.
-          </p>
-          <button
-            onClick={handleBackToLessons}
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-          >
-            Voltar às Lições
-          </button>
-        </div>
-      ) : (
-        <div className="units-container flex flex-col items-center">
-          {dataUnits.map((unit, unitIndex) => (
-            <div key={unitIndex} className="unit w-full mb-8">
-              <h2 className="text-xl text-center font-semibold mb-4">
-                Unidade {unit.unidade}
-              </h2>
-              <div className="lessons-container">
-                {unit.licoes.map((lesson, index) => {
-                  let alignmentClass = "justify-center";
-                  let paddingClass = "";
-
-                  const isEvenUnit = unit.unidade % 2 === 0;
-
-                  if (!isEvenUnit) {
-                    if (index === 1) {
-                      alignmentClass = "";
-                      paddingClass = "pl-24";
-                    } else if (index === 2) {
-                      alignmentClass = "justify-end";
-                      paddingClass = "pl-32";
-                    } else if (index === 3) {
-                      alignmentClass = "";
-                      paddingClass = "pl-24";
-                    }
-                  } else {
-                    if (index === 1) {
-                      alignmentClass = "flex-row-reverse";
-                      paddingClass = "pr-24";
-                    } else if (index === 2) {
-                      alignmentClass = "justify-start";
-                      paddingClass = "pr-32";
-                    } else if (index === 3) {
-                      alignmentClass = "flex-row-reverse";
-                      paddingClass = "pr-24";
-                    }
-                  }
-
-                  return (
-                    <div
-                      key={index}
-                      className={`lesson-row flex items-center my-4 ${alignmentClass} ${paddingClass}`}
-                    >
-                      <LessonNode
-                        lesson={lesson}
-                        onClick={handleLessonClick}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
+  return(
+    <main className='bg-[#F9F9F9] h-[100vh]'>
+      <div className='m-auto flex justify-center w-[375px]'>
+        <div className='flex flex-col w-full px-6 mt-11'>
+          <div className='flex justify-between text-sm'>
+            <h3>Bom dia Gustavo!</h3>
+            <Image src={Notificacoes} alt='Notificações' width={24} height={24}/>
+          </div>
+          <h1 className='text-3xl font-bold mt-3'>
+            Procure sua trilha
+          </h1>
+          <div className='flex gap-4 my-4'>
+            <input type="text" className='border rounded-md w-full'/>
+            <Image src={Filter} alt='Filter' width={44} height={44}/>
+          </div>
+          <div>
+            <div className='flex justify-between mb-2'>
+              <span className='text-xl'>Linguangens</span>
+              <span className='text-sm text-[#1A68FF]'>Ver todas</span>
             </div>
-          ))}
+            <div className='flex gap-[2%] flex-wrap'>
+              {dataTrilhas.map(trilha => (
+                <Link href={'/trilha'} key={trilha.id} className='h-[98px] w-[32%] bg-white flex items-end p-2 justify-center rounded-3xl mb-4 shadow-sm hover:scale-105 duration-300 cursor-pointer'>
+                  <span className='text-sm'>{trilha.nome}</span>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
-      )}
-    </div>
-  );
+        
+      <div className='flex justify-center'>
+        <div className='bg-white fixed h-16 bottom-10 w-[311px] left-[340px] shadow-2xl rounded-3xl flex'>
+          <div className='flex justify-around w-full py-2 items-center'>
+            <Image src={HomeIcon} alt='Home' width={48} height={48} className='h-[48px]'></Image>
+            <Image src={Chart} alt='Home' width={48} height={48} className='h-[30px] w-[30px]'></Image>
+            <Image src={Chat} alt='Home' width={48} height={48} className='h-[30px] w-[30px]'></Image>
+            <Image src={Profile} alt='Home' width={48} height={48} className='h-[30px] w-[30px]'></Image>
+          </div>
+        </div>
+      </div>
+      </div>
+    </main>
+  )
 }
---
+++ app/trilha/page.tsx
@@ -0,0 +1,166 @@
+"use client";
+import Image, { StaticImageData } from "next/image";
+import { useState } from "react";
+import Code from "@/public/code.png";
+import Book from "@/public/book.png";
+import Bau from "@/public/bau.png";
+import Start from "@/public/start.png";
+import BackArrow from "@/public/backArrow.png";
+import Link from "next/link";
+
+interface Lesson {
+  level: number;
+  image: StaticImageData;
+}
+
+interface Unit {
+  unidade: number;
+  licoes: Lesson[];
+}
+
+const dataUnits: Unit[] = [
+  {
+    unidade: 1,
+    licoes: [
+      { level: 1, image: Start },
+      { level: 2, image: Book },
+      { level: 3, image: Bau },
+      { level: 4, image: Code },
+      { level: 5, image: Code },
+    ],
+  },
+  {
+    unidade: 2,
+    licoes: [
+      { level: 6, image: Code },
+      { level: 7, image: Code },
+      { level: 8, image: Code },
+      { level: 9, image: Code },
+      { level: 10, image: Code },
+    ],
+  },
+  {
+    unidade: 3,
+    licoes: [
+      { level: 11, image: Code },
+      { level: 12, image: Code },
+      { level: 13, image: Code },
+      { level: 14, image: Code },
+      { level: 15, image: Code },
+    ],
+  },
+];
+
+interface LessonNodeProps {
+  lesson: Lesson;
+  onClick: (lesson: Lesson) => void;
+}
+
+const LessonNode = ({ lesson, onClick }: LessonNodeProps) => (
+  <div
+    onClick={() => onClick(lesson)}
+    className="lesson-node flex flex-col items-center mx-2 cursor-pointer"
+  >
+    <Image
+      src={lesson.image}
+      alt={`Lição ${lesson.level}`}
+      width={80}
+      height={80}
+    />
+  </div>
+);
+
+export default function Trilha() {
+  const [selectedLesson, setSelectedLesson] = useState<Lesson | null>(null);
+
+  const handleLessonClick = (lesson: Lesson) => {
+    setSelectedLesson(lesson);
+  };
+
+  const handleBackToLessons = () => {
+    setSelectedLesson(null);
+  };
+
+  return (
+    <div className="mt-10 w-full flex flex-col items-center">
+      <div className="flex w-[375px] pl-4 gap-2 mb-4">
+          <Link href={'./'}><Image src={BackArrow} alt="Back" className="w-[24px] h-[24px]"></Image>
+          </Link>
+          <span>JavaScript</span>
+      </div>
+
+      {selectedLesson ? (
+        <div className="lesson-content w-full max-w-md p-4">
+          <h2 className="text-xl font-semibold mb-4">
+            Conteúdo da Lição {selectedLesson.level}
+          </h2>
+          <p>
+            Este é o conteúdo da Lição {selectedLesson.level}. Aqui você pode
+            adicionar exercícios, explicações, exemplos de código ou qualquer
+            outro conteúdo relevante para esta lição.
+          </p>
+          <button
+            onClick={handleBackToLessons}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Voltar às Lições
+          </button>
+        </div>
+      ) : (
+        <div className="units-container flex flex-col items-center">
+          {dataUnits.map((unit, unitIndex) => (
+            <div key={unitIndex} className="unit w-full mb-8">
+              <h2 className="text-xl text-center font-semibold mb-4">
+                Unidade {unit.unidade}
+              </h2>
+              <div className="lessons-container">
+                {unit.licoes.map((lesson, index) => {
+                  let alignmentClass = "justify-center";
+                  let paddingClass = "";
+
+                  const isEvenUnit = unit.unidade % 2 === 0;
+
+                  if (!isEvenUnit) {
+                    if (index === 1) {
+                      alignmentClass = "";
+                      paddingClass = "pl-24";
+                    } else if (index === 2) {
+                      alignmentClass = "justify-end";
+                      paddingClass = "pl-32";
+                    } else if (index === 3) {
+                      alignmentClass = "";
+                      paddingClass = "pl-24";
+                    }
+                  } else {
+                    if (index === 1) {
+                      alignmentClass = "flex-row-reverse";
+                      paddingClass = "pr-24";
+                    } else if (index === 2) {
+                      alignmentClass = "justify-start";
+                      paddingClass = "pr-32";
+                    } else if (index === 3) {
+                      alignmentClass = "flex-row-reverse";
+                      paddingClass = "pr-24";
+                    }
+                  }
+
+                  return (
+                    <div
+                      key={index}
+                      className={`lesson-row flex items-center my-4 ${alignmentClass} ${paddingClass}`}
+                    >
+                      <LessonNode
+                        lesson={lesson}
+                        onClick={handleLessonClick}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
